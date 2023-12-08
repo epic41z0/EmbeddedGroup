44413ab87ef7d71ce6a6d92d691701986fbf0bf6
Sezzry <146092374+Sezzry@users.noreply.github.com>
2023-12-08 21:15:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -49,14 +49,14 @@ void displayMessage(const char *message, bool scroll = true, bool blink = true) 
         } else {
             int messageLength = strlen(message);
             if (messageLength > 16) {
-                lcd.setCursor(15, 0);
+                lcd.setCursor(0, 1);
                 for (int i = 16; i < messageLength + 16; ++i) {
                     lcd.write(message[i]);
                 }
             }
 
-            lcd.setCursor(0, 1);
-            lcd.print(" ");
+            lcd.setCursor(0, 0);
+            lcd.write(message);
             delay(2000);
         }
     }
@@ -65,7 +65,7 @@ void displayMessage(const char *message, bool scroll = true, bool blink = true) 
 void displayRandomMessage(const char *messages[], const bool scroll[], const bool blink[], int payment) {
     int len = sizeof(messages) / sizeof(messages[0]);
     int randomIndex = random(0, len);
-    displayMessage(messages[randomIndex], true, blink[randomIndex]);  // Always scroll the message
+    displayMessage(messages[randomIndex], true, blink[randomIndex]);  
     delay(2000);
 }
 
@@ -102,6 +102,7 @@ void setup() {
     delay(1000);  
     Serial.println("Initialization complete");
 
+   
     randomSeed(analogRead(0));
 }
 
@@ -151,22 +152,22 @@ void loop() {
     for (int i = 0; i < 29; i++) {
         switch (curreCustomer) {
             case 0:
-                Serial.println("Displaying Kop bil hos Harry");
+                Serial.println("Kop bil hos Harry");
                 Serial.print("Message: "); Serial.println(harryMessages[0]);
                 Serial.print("Scroll: "); Serial.println(harryScroll[0]);
                 Serial.print("Blink: "); Serial.println(harryBlink[0]);
                 displayMessage(harryMessages[0], harryScroll[0], harryBlink[0]);
                 break;
             case 1:
-                Serial.println("Displaying En god bilaffar (for Harry!)");
+                Serial.println("En god bilaffar(for Harry!)");
                 Serial.print("Message: "); Serial.println(harryMessages[1]);
                 Serial.print("Scroll: "); Serial.println(false);
                 Serial.print("Blink: "); Serial.println(false);
                 displayMessage(harryMessages[1], false, false);
                 break;
             case 2:
-                Serial.println("Displaying Hederlige Harrys Bilar");
-                Serial.print("Message: "); Serial.println(harryMessages[2]);
+                Serial.println("Hederlige Harrys Bilar");
+                Serial.print("Message: "); Serial.println(false);
                 Serial.print("Scroll: "); Serial.println(harryScroll[2]);
                 Serial.print("Blink: "); Serial.println(harryBlink[2]);
                 displayMessage(harryMessages[2], harryScroll[2], harryBlink[2]);
@@ -196,5 +197,5 @@ void loop() {
     lcd.print("Restarting...");
 
     Serial.println("Restarting...");
-    delay(5000);  
+    delay(5000); 
 }
